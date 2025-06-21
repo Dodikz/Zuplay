03fc4d07f998b5e3952dd03ab910e1eddeda16f5
Dodik Vexel <sabdoilahidodik735@gmail.com>
2025-06-21 14:04:39 +0700

    test

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'zuplay.up.railway.app:9000'),
 
     /*
     |--------------------------------------------------------------------------
